f5c06266ad6865ebd5604df550e519c5a44596ad
MohammadMaasir <maasir554@gmail.com>
2023-09-17 01:13:09 +0530

    Homework on arrays

--- 47-all-pairs.cpp
+++ 47-all-pairs.cpp
@@ -11,6 +11,16 @@ void printAllPairs(int arr[], int size){
     return;
 }
 
+void doubletSum(int arr[], int size){
+    for (int i = 0; i <size; i++){
+        for (int j = 0; j < size; j++){
+            cout << arr[i] + arr[j] << " ";
+        }
+        cout << endl;
+    }
+    return;
+}   
+    
 int main(){
     int sz;
     cout << "Enter size of array: "; cin >> sz;
@@ -18,5 +28,7 @@ int main(){
     cout << "Enter elements of the array (space separated) : ";
     for (int i = 0; i < sz; i++ ) cin >> brr[i];
     printAllPairs(brr, 3);
+    cout << "------------------------\n";
+    doubletSum(brr, sz);
     return 0;
 }
--- 48-all-triplets.cpp
+++ 48-all-triplets.cpp
@@ -5,7 +5,7 @@ using namespace std;
 void printAllTriplets(int arr[], int size){
     for (int i = 0; i < size; i++){
         for (int j = 0; j < size; j++){
-            for (int k = 1; k < size; k++){
+            for (int k = 0; k < size; k++){
                 printf("(%d, %d, %d) ",arr[i],arr[j],arr[k]);
             }
         }
@@ -15,8 +15,24 @@ void printAllTriplets(int arr[], int size){
     return;
 }
 
+void tripletSum(int arr[], int size){
+
+    for (int i = 0; i < size; i++){
+        for (int j = 0; j < size; j++){
+            for (int k = 0; k < size; k++){
+                cout << arr[i] + arr[j] + arr[k] << " ";
+            }
+        }
+        cout << endl;
+    }
+
+
+    return;
+}
+
 int main() {
     int brr[3] = {1,2,3};
     printAllTriplets(brr,3);
+    tripletSum(brr,3);
     return 0;
 }
